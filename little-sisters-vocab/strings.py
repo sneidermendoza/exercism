8c5ae5d55ef0a44887a91e3b12c466f5325abf10
sneidermendoza <mariasol0304@gmail.com>
2022-01-05 18:59:50 -0500

    terminado ejercicio little-sister

--- little-sisters-vocab/strings.py
+++ little-sisters-vocab/strings.py
@@ -1,3 +1,7 @@
+"""
+Little Sister's Vocabulary
+"""
+import string
 def add_prefix_un(word):
     """
     This function takes `word` as a parameter and
@@ -9,7 +13,6 @@ def add_prefix_un(word):
 
 def make_word_groups(vocab_words):
     """
-
     :param vocab_words: list of vocabulary words with a prefix.
     :return: str of prefix followed by vocabulary words with
              prefix applied, separated by ' :: '.
@@ -18,26 +21,17 @@ def make_word_groups(vocab_words):
     with the prefix  and the words with prefix applied, separated
      by ' :: '.
     """
-    
     return f" :: {vocab_words[0]}".join(vocab_words)
-
 
 def remove_suffix_ness(word):
     """
-
     :param word: str of word to remove suffix from.
     :return: str of word with suffix removed & spelling adjusted.
 
     This function takes in a word and returns the base word with `ness` removed.
     """
-    remove = word[:-4]
-    remove_list = list(remove)
-    if remove_list[-1] == "i":
-        remove_list[-1] = "y"
-        new = "".join(remove_list)
-        return new
-    else:
-        return remove   
+    return root if not (root:=word[:-4]).endswith("i") else root[:-1] + "y"
+
 
 def noun_to_verb(sentence, index):
     """
@@ -51,14 +45,5 @@ def noun_to_verb(sentence, index):
     is split apart.  The function should return the extracted
     adjective as a verb.
     """
-    suffix = "en"
-    index = int(index)
-    new_list = sentence.split()
-    verb = new_list[index]
-    verb_list = list(verb)
-    if verb_list[-1] == ".":
-        verb_list[-1] = "en"
-        new_verb = "".join(verb_list)
-        return new_verb
-    else:
-        return f"{verb}{suffix}"
+    return root + "en" if not (root:=sentence.split(" ")[index])[-1] in string.punctuation else root[:-1] + "en"
+    
